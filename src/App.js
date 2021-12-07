de5cf88045167020a61f643fcde9d8691c28bcbd
Anton <nyagolov.anton@gmail.com>
2021-12-08 00:49:02 +0200

    Changes to App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>ASD!</h1>
-      </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
     </>
   );
 }
