0ad397e261e2b480975b68b3bd121ccc669be824
Anton <nyagolov.anton@gmail.com>
2021-12-08 23:50:38 +0200

    Added services, sign-up and products to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Services from "./pages/Services";
+import SignUp from "./pages/SignUp";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </Router>
     </>
