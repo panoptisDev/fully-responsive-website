b193e4e82d6c233268d6b9a82b00fac12f987ab4
Anton <nyagolov.anton@gmail.com>
2021-12-09 22:24:14 +0200

    Added Footer in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../App.css";
 import Cards from "../../components/Cards";
+import Footer from "../../components/Footer";
 import HeroSection from "../../components/HeroSection";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 }
